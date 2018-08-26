62ef736ee92b07e1fa7ddcced9cd5d4b472af78c
GUYKLEIN1 <MRGUYKLEIN@GMAIL.COM>
2018-08-26 09:43:12 +0300

    added paradigm project
    paradigm project with changes.
    to run on windows you need to download and install ASIO4ALL.
    
    lines that work with asio are currently comment out.

--- sendLogFile.m
+++
@@ -1,9 +0,0 @@
-function sendLogFile()
-
-    global cfg;
-    
-    logFileAsChar = fileread(cfg.LOG_PREFIX);
-    fwrite(cfg.client_data_socket, logFileAsChar);
-    fprintf(cfg.logfile,'%f DATA COMPUTER- CLIENT_SOCKET_SENT PORT:%d\n',GetSecs,cfg.DATA_COMPUTER_PORT);
-   
-end
